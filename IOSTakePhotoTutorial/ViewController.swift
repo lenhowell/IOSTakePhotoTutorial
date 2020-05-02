596118eb8893c6223f26d087484d1c6c08b8b5b5
Lenard Howell <len.howell@gmail.com>
2020-05-01 23:02:40 -0400

    First Commit

--- IOSTakePhotoTutorial/ViewController.swift
+++ IOSTakePhotoTutorial/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+//var imagePicker: UIImagePickerController!
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a Source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion:nil)
+            } else {
+                print("Camera NOT Availaqble!!!")
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action: UIAlertAction) in imagePickerController.sourceType = .photoLibrary
+        self.present(imagePickerController, animated: true, completion:nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion:nil)
+
+ //       imagePicker =  UIImagePickerController()
+ //       imagePicker.delegate = self
+ //       imagePicker.sourceType = .photoLibrary
+
+ //       present(imagePicker, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        imageView.image = info[.originalImage] as? UIImage
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true, completion: nil)
+    }
 
 }
 
